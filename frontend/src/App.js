c7f3735c666883c60c360e7e10f5b21eab8bc0fd
Joshua Soto <joshua526@hotmail.com>
2022-11-12 17:01:09 -0600

    First

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Mercadet</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <div className="product-info">
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
